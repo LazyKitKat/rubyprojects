f653af972d4a8a257357739a10ded3e465c8bf70
LazyKitKat <lazykitkat@protonmail.com>
2022-03-08 15:24:52 +0100

    Fixed recursion

--- Recursion/main.rb
+++ Recursion/main.rb
@@ -5,9 +5,7 @@ def merge_sort(array)
     split_one = merge_sort(array.slice!(0, array.length/2))
     split_two = merge_sort(array)
 
-    p split_one
-    p split_two
-
+ 
     sorted = []
 
     until split_one.empty? or split_two.empty?
@@ -17,12 +15,9 @@ def merge_sort(array)
             sorted << split_two.shift
         end
     end
-    puts "Sorted:"
-    p sorted
-    puts
-    p sorted + split_one + split_two
+    sorted + split_one + split_two
 end
 
 arr = [3, 54, 2,1, 75]
-merge_sort(arr)
+p merge_sort(arr)
 
